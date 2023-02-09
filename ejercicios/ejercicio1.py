4af686d739d3b01591a2da487a9ca018313cb8f7
Bas <bysebashdez@gmail.com>
2023-02-09 06:47:58 -0500

    Exercise 1 done and add exercise 2

---
+++ ejercicios/ejercicio1.py
@@ -0,0 +1,7 @@
+#1. Crear un programa que pida al usuario ingresar su nombre y edad y los imprima en pantalla.
+
+age = int(input("How old are you?: "))
+
+name = input("What is your name?: ")
+
+print(f"Hello {name}, you are {age} years old")
