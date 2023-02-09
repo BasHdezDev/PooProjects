0084713238da3751965d619bf31e1537750cf87f
Bas <bysebashdez@gmail.com>
2023-02-09 09:41:57 -0500

    Nice try of solving exercise 7 and rest of files added

---
+++ ejercicios/exercise7.py
@@ -0,0 +1,13 @@
+#7. Escribir un programa que encuentre el número más grande y el más pequeño en una lista dada.
+
+list = [24,1,14,26,40]
+
+index = len(list)
+
+aux = 0
+
+for i in range(0,index,+1):
+    if list[aux]>list[i]:
+
+
+
