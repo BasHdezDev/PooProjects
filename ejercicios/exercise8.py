5445610a9dfa333244f2974c81b363a231d399c3
Bas <bysebashdez@gmail.com>
2023-02-09 19:23:33 -0500

    Exercise 8 done

---
+++ ejercicios/exercise8.py
@@ -0,0 +1,10 @@
+#8. Crear una función que invierta el orden de los elementos en una lista dada.
+
+List = [15,2,6,5,4,8,7,6,6,6,6,6,259784,1]
+
+def reverse(List):
+    List.reverse()
+    return print(f"The reverse of the list is {List}")
+
+
+reverse(List)
